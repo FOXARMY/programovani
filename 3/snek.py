25f17586283917fd1477b52681adca4f1296f7b1
BONBON <Spackova.Hana.13@glowackeho.eu>
2018-11-29 18:20:57 +0100

    Vyrobila jsem sneka.

---
+++ 3/snek.py
@@ -0,0 +1,36 @@
+from turtle import forward, left, right, exitonclick
+
+for cislo in range(76):
+	forward(cislo / 3)
+	right(11)
+
+left(90)
+forward(100)
+left(55)
+forward(50)
+
+left(90)
+
+for cislo in range(30):
+	forward(3)
+	right(11)
+
+left(90)
+forward(45)
+left(90)
+forward(30)
+
+for cislo in range(15):
+	forward(3)
+	right(11)
+
+for cislo in range(25):
+	forward(10)
+	left(15)
+	forward(10)
+	right(15)
+	
+right(145)
+forward(160)
+	
+exitonclick()
